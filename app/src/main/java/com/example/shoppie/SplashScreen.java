4d52db303152437ee2d9dc7b44d8a1175eb0cad6
deepak-kumar <deepakvickykumar360@gmail.com>
2024-04-19 11:24:30 +0530

    developed splash screen

---
+++ app/src/main/java/com/example/shoppie/SplashScreen.java
@@ -0,0 +1,14 @@
+package com.example.shoppie;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
