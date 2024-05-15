fb2d05bd4e0c7178276c10113992e8a1f30d063b
deepak-kumar <deepakvickykumar360@gmail.com>
2024-05-15 20:19:48 +0530

    finilized UI of create_account and Login_page

--- app/src/main/java/com/example/shoppie/CreateAccountActivity.java
+++ app/src/main/java/com/example/shoppie/CreateAccountActivity.java
@@ -1,6 +1,9 @@
 package com.example.shoppie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,20 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
+    Button cancelButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cancelButton = findViewById(R.id.btnCancleCreateAccount);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_to_splashScreen = new Intent(CreateAccountActivity.this,SplashScreen.class);
+                startActivity(go_to_splashScreen);
+
+            }
         });
     }
 }
--- app/src/main/java/com/example/shoppie/LoginActivity.java
+++ app/src/main/java/com/example/shoppie/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.shoppie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button cancelLoginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        cancelLoginBtn = findViewById(R.id.btnCancleLogin);
+        cancelLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_to_SplashScreen = new Intent(LoginActivity.this, SplashScreen.class);
+                startActivity(go_to_SplashScreen);
+            }
         });
+
     }
 }
