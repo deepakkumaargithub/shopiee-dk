1e1f4ea45ac573211789d0de8dc1a7648e43a852
Deepak Kumar <deepakvickykumar360@gmail.com>
2024-07-06 00:34:13 +0530

    add image slider for product

--- app/src/main/java/com/example/shoppie/ProductImageAdapter.java
+++ app/src/main/java/com/example/shoppie/ProductImageAdapter.java
@@ -9,35 +9,46 @@ import androidx.viewpager.widget.PagerAdapter;
 
 import java.util.List;
 
-public class ProductImageAdapter extends PagerAdapter{
+public class ProductImageAdapter extends PagerAdapter {
 
     private List<Integer> productImages;
 
-    public ProductImageAdapter(List<Integer> productImages){
+    public ProductImageAdapter(List<Integer> productImages) {
         this.productImages = productImages;
     }
 
     @NonNull
     @Override
     public Object instantiateItem(@NonNull ViewGroup container, int position) {
-        ImageView productImage = new ImageView(container.getContext());  //create image view
-        productImage.setImageResource(productImages.get(position)); // set resources for image
-        container.addView(productImage,0);   // added in container
+        ImageView productImage = new ImageView(container.getContext()); // Create ImageView
+        productImage.setImageResource(productImages.get(position)); // Set image resource
+
+        // Add content description for accessibility
+        productImage.setContentDescription("Product image " + (position + 1));
+
+        // Set layout parameters to match parent (optional, if not set through XML)
+        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT
+        );
+        productImage.setLayoutParams(layoutParams);
+
+        container.addView(productImage); // Add ImageView to container
         return productImage;
     }
 
     @Override
     public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
-        container.removeView((ImageView)object);  //remove image view
+        container.removeView((ImageView) object); // Remove ImageView from container
     }
 
     @Override
     public int getCount() {
-        return productImages.size();  // list size
+        return productImages.size(); // Return size of the images list
     }
 
     @Override
     public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
-        return view == object;
+        return view == object; // Return if view equals object
     }
 }
