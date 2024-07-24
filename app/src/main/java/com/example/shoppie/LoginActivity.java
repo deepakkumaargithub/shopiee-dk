fc13ea542f6726d3349e1ab1408fe77221e43e59
Deepak Kumar <deepakvickykumar360@gmail.com>
2024-07-25 01:43:46 +0530

    added shared prefences

--- app/src/main/java/com/example/shoppie/LoginActivity.java
+++ app/src/main/java/com/example/shoppie/LoginActivity.java
@@ -1,11 +1,13 @@
 package com.example.shoppie;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -24,6 +26,7 @@ public class LoginActivity extends AppCompatActivity {
 
     EditText loginEmailOrPhoneNo, loginPassword;
     Button cancelLoginBtn, signInButton;
+    TextView createAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         loginPassword = findViewById(R.id.password_text);
         signInButton = findViewById(R.id.sign_in_btn);
         cancelLoginBtn = findViewById(R.id.btnCancleLogin);
+        createAccount = findViewById(R.id.CA_txt);
 
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +53,15 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent go_to_SplashScreen = new Intent(LoginActivity.this, SplashScreen.class);
                 startActivity(go_to_SplashScreen);
+                finish(); // Optional: Close LoginActivity to prevent going back to it from SplashScreen
+            }
+        });
+
+        createAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent go_to_createAccount = new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(go_to_createAccount);
                 finish(); // Optional: Close LoginActivity to prevent going back to it from SplashScreen
             }
         });
@@ -70,9 +83,18 @@ public class LoginActivity extends AppCompatActivity {
                             // Passwords match, authentication successful
                             Log.d(TAG," Passwords match, authentication successful");
                             Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+
+                            //saving login state
+                            SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putBoolean("isLoggedIn", true);
+                            editor.apply();
+
                             Intent intent = new Intent(LoginActivity.this, UserLocationPermissionActivity.class);
                             startActivity(intent);
                             finish(); // Close LoginActivity after successful login
+
+
                         } else {
                             // Passwords do not match
                             Log.d(TAG," Password do not match");
